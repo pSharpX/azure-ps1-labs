61b01dfb58cc8aeca3ee50ab9f8e8ffafa58605c
Christian Rivera <crivera@loyalty.com>
2023-11-07 09:08:49 -0500

    feat(fundamentals): added script for creating objects using powershell

---
+++ fundamentals/Objects.ps1
@@ -0,0 +1,16 @@
+# Objects & Arrays
+
+$MyArray = @(10, 20, 5, 40, 100, "Hellow", "World")
+$MyHashTable = @{
+    "FirstName" = "Jhon"
+    "LastName" = "Snow"
+    "Age" = 32
+    "DateOfBirth" = "1988/10/10"
+    "Country" = "England"
+}
+
+$Metadata = @{"Technical-Owner" = "TeamPayments"; Environment = "Development"; Provisioner = "PowerShell"; "Application-Id" = "QR Payments"; "Data-Classification" = "Classified"}
+
+$MyArray
+$MyHashTable
+$Metadata

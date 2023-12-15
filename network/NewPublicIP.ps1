c424f84538019078099abed12549692cc97c5204
Christian Rivera <crivera2093@gmail.com>
2023-12-15 17:09:24 -0500

    feat: added script for creating new public ip address.

---
+++ network/NewPublicIP.ps1
@@ -0,0 +1,21 @@
+$ApplicationId = "qrpayments"
+$Tags = @{
+    Provisioner = "PowerShell"
+    Environment = "Development"
+    "Technical-Owner" = "TeamDragons"
+    "Application-Id" = $ApplicationId
+    "Data-Classification" = "Classified"
+}
+
+$ResourceGroupName = "TeamDragons_rg"
+$ResourceGroup = Get-AzResourceGroup -Name $ResourceGroupName
+$Location = $ResourceGroup.Location
+
+$PublicIPName = "pip" + $ApplicationId
+$CommonProps = @{
+    ResourceGroupName = $ResourceGroupName
+    Location = $Location
+    Tag = $Tags
+}
+
+$PublicIPAddress = New-AzPublicIpAddress @CommonProps -Name $PublicIPName -Sku "Standard" -AllocationMethod "Static"

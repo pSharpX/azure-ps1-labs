5b1520cfe4a15e57468098a7b2d4f1106ed807c8
Christian Rivera <crivera2093@gmail.com>
2023-12-27 18:54:44 -0500

    feat: list windows vm image publishers

--- virtual_machines/GetWindowsVMImages.ps1
+++ virtual_machines/GetWindowsVMImages.ps1
@@ -4,13 +4,40 @@ Get-AzVMSize -Location $Location
 $VMSizes = Get-AzVMSize -Location $Location
 $VMSizes | Where-Object {$_.Name -eq "Standard_DS2_v2"}
 
-$PublisherName = "MicrosoftWindowsServer"
+<# Some Windows Image Publishers:
+MicrosoftWindowsDesktop
+MicrosoftWindowsServer
+#>
+$PublisherName = "MicrosoftWindowsDesktop" 
 Get-AzVMImagePublisher -Location $Location | Where-Object {$_.PublisherName -eq $PublisherName}
+#Get-AzVMImagePublisher -Location $Location | Where-Object {$_.PublisherName -like "*$PublisherName*"}
 
-Get-AzVMImageOffer -Location $Location -PublisherName $PublisherName
+<# Some Offers for "MicrosoftWindowsDesktop" Publisher:
+office-365
+Windows-10
+windows-10-20h2-vhd-client-prod-stage
+windows-11
+windows-7
+windows-ent-cpc
+windows10preview
+windows11preview
+windows11preview-arm64
 
-$Offer = "WindowsServer"
+Some Offers for "MicrosoftWindowsServer" Publisher:
+servertesting
+windows-cvm
+WindowsServer
+windowsserver-gen2preview
+windowsserver-previewtest
+windowsserverdotnet
+windowsserverhotpatch-previews
+WindowsServerSemiAnnual
+windowsserverupgrade
+#>
+Get-AzVMImageOffer -Location $Location -PublisherName $PublisherName | Select-Object -Property Offer
+
+$Offer = "windows-11" #"WindowsServer"
 Get-AzVMImageSku -Location $Location -PublisherName $PublisherName -Offer $Offer
 
-$Sku = "2019-DataCenter"
+$Sku = "win11-23h2-pro" #"2019-DataCenter"
 Get-AzVMImage -Location $Location -PublisherName $PublisherName -Offer $Offer -Skus $Sku

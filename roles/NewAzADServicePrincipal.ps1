1c912ab5d7a19ad52c0dea2d9e7bf14b18bf5438
Christian Rivera <crivera2093@gmail.com>
2024-01-05 23:58:21 -0500

    feat: include tags to service principal

--- roles/NewAzADServicePrincipal.ps1
+++ roles/NewAzADServicePrincipal.ps1
@@ -2,13 +2,20 @@ Disable-AzContextAutosave
 Connect-AzAccount
 
 $ApplicationId = "QrPayments"
+$Tags = @{
+    Provisioner = "PowerShell"
+    Environment = "Development"
+    "Technical-Owner" = "TeamDragons"
+    "Application-Id" = $ApplicationId
+    "Data-Classification" = "Restricted"
+}
 
 Write-Host "Get or Create new Service Principal"
 $ServicePrincipalName = "$($ApplicationId)App"
 $ServicePrincipal = Get-AzADServicePrincipal -DisplayName $ServicePrincipalName
 if ($null -eq $ServicePrincipal) {
     Write-Host "Service Principal not found. $($ServicePrincipalName) SP will be created"
-    $ServicePrincipal = New-AzADServicePrincipal -DisplayName $ServicePrincipalName
+    $ServicePrincipal = New-AzADServicePrincipal -DisplayName $ServicePrincipalName -Tag $Tags
 }
 
 $ServicePrincipalId = $ServicePrincipal.Id

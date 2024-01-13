a4ab1c6399efb87d982acc1ae27b8ba824eb39c6
Christian Rivera <crivera2093@gmail.com>
2024-01-13 00:57:17 -0500

    feat: added useful notes about vm agent logs

---
+++ azure-commands.sh
@@ -0,0 +1,4 @@
+#When running CustomExtensionScripts or RunCommands on Virtual Machines to see executed code go to the following log files in agent folder:
+# /var/lib/waagent/custom-script/download/0/install_ansible.sh
+# /var/lib/waagent/run-command/download/0/script.sh
+# /var/lib/waagent/run-command-handler/download/ansiblevmqrpaymentsansiblecommand/0/script.sh
